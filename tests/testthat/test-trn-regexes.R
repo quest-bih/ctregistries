4c735eb250776a993b5f321a0c2f1320281efc01
Nachev <vladislav.nachev@charite.de>
2025-04-24 10:43:14 +0200

    code review miniupdate

--- tests/testthat/test-trn-regexes.R
+++ tests/testthat/test-trn-regexes.R
@@ -145,7 +145,7 @@ test_that("EudraCT TN rejected", {
   tests <- c(
     # "200400600800", #10.1007/s00394-015-1084-x [ISSUE]
     # "201201801201"  #10.1016/j.jpain.2012.01.003 [ISSUE]
-    "Name: EU Trial Number Number: 2022-502482-17-00", # CTIS replaced EudraCT
+    "Name: EU Trial Number Number: 2022-502482-17-00", # This is a CTIS TRN, not EudraCT
     "PACTR201205000384379" #PMC5278894 (previously matched because of second "20")
   )
 
